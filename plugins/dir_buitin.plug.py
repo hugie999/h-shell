9bc10886642b3ac6eaab7c16d5b8a7cf0721ca34
hugie999 <hugobohacek@gmail.com>
2023-03-27 22:37:03 -0230

    add cool outline to ls/dir commands when its a dir

--- plugins/dir_buitin.plug.py
+++ plugins/dir_buitin.plug.py
@@ -6,19 +6,21 @@ META = {
     "ver" : 1
 }# note plugin ver and ver are DIFFRENT ver is for the version of the plugin and plugin ver is what is used in the docom function
 PLUGVER = 1 #this is for compatibility or somthing
-import pathlib as p
+from pathlib import Path
 import os
-def docom(comfull,themestr,cd):
+def docom(comfull,themestr,cd= Path(__file__)):
     hi = os.get_terminal_size()[1]
-    print("listing of" + str(cd))
+    print("listing of " + str(cd))
     a = 0
     for i in cd.iterdir():
         a += 1
-        print(themestr[0]+str(i))
+        if i.is_dir():
+            print(themestr[1]+str(i) + " -[dir]-"+themestr[0])
+        else:
+            print(themestr[0]+str(i))
         if a == hi - 2:
             input(themestr[1]+"--press enter to show more--"+themestr[0])
             print("\x1B[1A",end="")
             print("\x1B[2K",end="")
             a = 0
-    
     #print(themestr[1]+"hello world im a plugin lol")
