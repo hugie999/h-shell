b117907a6e3d5f8b6eeeffe5b91b52d25f4ab5b4
hugie999 <hugobohacek@gmail.com>
2023-06-19 14:22:03 -0230

    add find to dir plugin

--- plugins/dir_buitin.plug.py
+++ plugins/dir_buitin.plug.py
@@ -1,4 +1,4 @@
-COMS = ["ls","dir"] #commands used
+COMS = ["ls","dir","find"] #commands used
 META = {
     "name": "built in dir and ls command",
     "desc": "shows the directory of the current directory",
@@ -6,8 +6,8 @@ META = {
     "ver" : 1
 }# note pluginver and ver are DIFFRENT pluginver is for the version of the plugin and ver is what is used in the docom function
 PLUGVER = 1 #this is for compatibility or somthing
-HELPCOMS = ["ls/dir"]
-HELPDESC = ["shows files in the current directory"]
+HELPCOMS = ["ls/dir","find [querey]"]
+HELPDESC = ["shows files in the current directory","shows files with [querey] in thare names"]
 #note2 type is the well type of plugin
 #type 0 is the normal one and is only called when a reserved command is used
 #type 1 is called every command and the used command is also run after
@@ -23,34 +23,66 @@ def docom(comfull="",themestr=[],cdreal= Path(__file__)):
     
     
     
-    if len(comfull.split()) > 1:
-        #print(comfull)
-        cd = Path(comfull.split()[len(comfull.split())-1])
-    else:
-        cd = cdreal
-    try:
-        hi = os.get_terminal_size()[1]
-        if hasmeta:
-            print(str(themestr[1]+"---"+"listing of " + str(metaname)+"---"+themestr[0]).ljust(os.get_terminal_size()[0]))
+    if comfull.split()[0] == "ls" or comfull.split()[0] == "dir":
+        if len(comfull.split()) > 1:
+            #print(comfull)
+            cd = Path(comfull.split()[len(comfull.split())-1])
         else:
-            print(str(themestr[1]+"---"+"listing of " + str(cd)+"---"+themestr[0]).ljust(os.get_terminal_size()[0]))
-        a = 0
-        for i in cd.iterdir():
-            a += 1
-            if i.is_dir():
-                print(themestr[1]+str(i.name) + " -[dir]-"+themestr[0])
+            cd = cdreal
+        try:
+            hi = os.get_terminal_size()[1]
+            if hasmeta:
+                print(str(themestr[1]+"---"+"listing of " + str(metaname)+"---"+themestr[0]).ljust(os.get_terminal_size()[0]))
             else:
-                if i.name != ".hmeta" and i.name != ".hdrvmeta  ":
-                    print(themestr[0]+str(i.name))
-            
-            if a == hi - 1 or a == hi:
-                input(themestr[1]+"--press enter to show more--"+themestr[0])
-                print("\x1B[1A",end="")
-                print("\x1B[2K",end="")
-                a = 0
-        #print(themestr[1]+"hello world im a plugin lol")
-    except KeyboardInterrupt:
-        pass
-    #print("\x1B[1A",end="")
-    print("\x1B[2K",end="")
-    print("\x1B[0E",end="")
+                print(str(themestr[1]+"---"+"listing of " + str(cd)+"---"+themestr[0]).ljust(os.get_terminal_size()[0]))
+            a = 0
+            for i in cd.iterdir():
+                a += 1
+                if i.is_dir():
+                    print(themestr[1]+str(i.name) + " -[dir]-"+themestr[0])
+                else:
+                    if i.name != ".hmeta" and i.name != ".hdrvmeta  ":
+                        print(themestr[0]+str(i.name))
+                
+                if a == hi - 1 or a == hi:
+                    input(themestr[1]+"--press enter to show more--"+themestr[0])
+                    print("\x1B[1A",end="")
+                    print("\x1B[2K",end="")
+                    a = 0
+            #print(themestr[1]+"hello world im a plugin lol")
+        except KeyboardInterrupt:
+            pass
+        #print("\x1B[1A",end="")
+        print("\x1B[2K",end="")
+        print("\x1B[0E",end="")
+    elif comfull.split()[0] == "find":
+        searchfor = comfull[5:]
+        # if len(comfull.split()) > 1:
+        #     #print(comfull)
+        #     cd = Path(comfull.split()[len(comfull.split())-1])
+        
+        cd = cdreal
+        try:
+            hi = os.get_terminal_size()[1]
+            print(str(themestr[1]+"---"+"searching for " + searchfor +"---"+themestr[0]).ljust(os.get_terminal_size()[0]))
+            a = 0
+            for i in cd.iterdir():
+                if searchfor in i.name:
+                    a += 1
+                    if i.is_dir():
+                        print(themestr[1]+str(i.name) + " -[dir]-"+themestr[0])
+                    else:
+                        if i.name != ".hmeta" and i.name != ".hdrvmeta  ":
+                            print(themestr[0]+str(i.name))
+                    
+                    if a == hi - 1 or a == hi:
+                        input(themestr[1]+"--press enter to show more--"+themestr[0])
+                        print("\x1B[1A",end="")
+                        print("\x1B[2K",end="")
+                        a = 0
+        except KeyboardInterrupt:
+            pass
+        #print("\x1B[1A",end="")
+        print("\x1B[2K",end="")
+        print("\x1B[0E",end="")
+        
