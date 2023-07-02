e7bf3c48e0ae02c68e3bd79b264d32297e6a3b2f
hugie999 <hugobohacek@gmail.com>
2023-07-02 01:17:32 -0230

    make it so readline dosent crash on windows

--- plugins/readline.plug.py
+++ plugins/readline.plug.py
@@ -10,23 +10,36 @@ META = {
 #type 0 is the normal one and is only called when a reserved command is used
 #type 1 is called every command and the used command is also run after
 PLUGVER = 1 #this is for compatibility or somthing
-HELPCOMS = []
-HELPDESC = []
+HELPCOMS = ["hist"]
+HELPDESC = ["shows history"]
 from pathlib import Path
 
 histfile = str(Path("~/.HSHhist").expanduser())
 import os
 wi = os.get_terminal_size().columns
-import readline
+iswin = os.name == "nt"
+if iswin:
+    print("!!!WINDOWS DETECTED!!!")
+    print("     please delete    ")
+    print("    readline plugin   \n")
+else:
+    import readline
 try:
     readline.read_history_file(histfile)
 except FileNotFoundError:
     Path(histfile).touch()
     readline.write_history_file(histfile)
+except NameError:
+    print("!!!readline not imported (name error)!!!\n")
 def oncommand(comfull,themestr,cd):
+    if iswin:
+        return
     readline.write_history_file(histfile)
 def docom(comfull,themestr,cd):
-    
+    if iswin:
+        print("!!!WINDOWS DETECTED!!!")
+        print("readline plugin does\nnot work on windows\nplease delete this plugin")
+        return
     print(themestr[1]+"--history--".center(wi,"-")+themestr[0])
     f = open(histfile)
     for i in f.read().split("\n"):
